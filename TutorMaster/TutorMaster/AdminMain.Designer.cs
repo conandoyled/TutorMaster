852b855ec9107e6ce9d9464b26f307a6ccd6888d
unknown <asmarek@SH301B-02.coe.int>
2017-03-03 12:25:23 -0600

    Added functionality to Admin Delete button.

--- TutorMaster/TutorMaster/AdminMain.Designer.cs
+++ TutorMaster/TutorMaster/AdminMain.Designer.cs
@@ -95,6 +95,7 @@
             this.btnDelete.TabIndex = 4;
             this.btnDelete.Text = "Delete Student";
             this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
             // 
             // btnCreateStudent
             // 
