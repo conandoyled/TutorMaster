9675a1b7cd89ca549acd0facf04ba5aab6e083b4
unknown <jlgarcia@SH307-05.coe.int>
2017-03-14 15:17:51 -0500

    Made some changes, able to identify proper requests, populating and formatting on the way

--- TutorMaster/TutorMaster/FacultyMain.cs
+++ TutorMaster/TutorMaster/FacultyMain.cs
@@ -24,16 +24,33 @@ namespace TutorMaster
         {
             //1. find which classes the faculty members are qualified to approve tutors for
             TutorMasterDBEntities4 db = new TutorMasterDBEntities4(); //create a new indirect entity to look at db
-            TutorMasterDBEntities4 dbt = new TutorMasterDBEntities4();
-            var Classes = (from row in db.FacultyClasses where row.FacultyID == accID select row.ClassCode); //This should pull out all the classes that the faculty member can approve tutors for
 
-            //2. Look at all pending requests and see if they are for a class from the faculty embers department
-   
-            foreach (var x in Classes)
-            {;}//use if maybe
-
+            var ApprovableClasses = (from row in db.FacultyClasses where row.FacultyID == accID select row.ClassCode); //This should pull out all the classes that the faculty member can approve tutors for
+            List<String> AC= new List<String>(); //create a list to store the DB info that was just pulled
+            AC= ApprovableClasses.ToList<String>(); //Fill the list
             
-            //3. If they are, add them to the box
+            var PendingRequests = (from row in db.TutorRequests select row); //This pulls out the whole table of pending requests
+            List<TutorRequest> PC = new List<TutorRequest>(); //create a list to store the DB info that was just pulled
+            PC = PendingRequests.ToList<TutorRequest>(); //Fill the list
+
+            //1.5 Create an empty list to fill with the right requests in step 2
+            List<TutorRequest> FC = new List<TutorRequest>(); //here is that beautiful empty list
+
+            //2. Look at all pending requests and see if they are for a class from the faculty members department. If they are, add them to the box
+            
+            foreach(String classcode in AC) //Iterate through all of the class  codes and check them
+            {
+                foreach (TutorRequest tut in PC) // Iterate through all of the requests
+                {
+                    if (classcode == tut.ClassCode) // If the class code that we are checking matches the class code of the object,
+                    {
+                        FC.Add(tut); // Then add that tutor request to the list of requests to display
+                    }
+                }
+            }
+
+            //3. Now that all the requests are in the FC, we need to display each of the tutor requests in clbPendingReequests in the form of [STUDENT NAME, CLASS TO TUTOR]
+            //3.5 I have to go through the student table and pull out the names of the people in it 
         }
 
         private void FacultyMain_FormClosed(object sender, FormClosedEventArgs e)
