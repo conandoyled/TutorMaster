d6587c72a5c3ca6d351bd267950ed6176e6f8cc0
unknown <mjbradley@SH301B-01.coe.int>
2017-03-20 20:35:13 -0500

    QuickSort is working!

--- TutorMaster/TutorMaster/StudentMain.cs
+++ TutorMaster/TutorMaster/StudentMain.cs
@@ -12,7 +12,7 @@ namespace TutorMaster
     public partial class StudentMain : Form
     {
         private int id;
-
+        private int count = 0;
         public StudentMain(int accID)
         {
             id = accID;
@@ -26,11 +26,17 @@ namespace TutorMaster
             TutorMasterDBEntities4 db = new TutorMasterDBEntities4();
             var studentCommits = (from row in db.StudentCommitments.AsEnumerable() where row.ID == id select row.CmtID).ToArray();
             DateTime start = new DateTime(2017, 1, 1, 0, 0, 0);
-            List<DateTime> dateList = new List<DateTime>();
+            List<TutorMaster.Commitment> cmtList = new List<TutorMaster.Commitment>();
             for (int j = 0; j < studentCommits.Count(); j++)
             {
                 TutorMaster.Commitment commit = (from row in db.Commitments.AsEnumerable() where row.CmtID == studentCommits[j] select row).First();
-                dateList.Add(Convert.ToDateTime(commit.StartTime));
+                cmtList.Add(commit);
+            }
+            QuickSort(ref cmtList, cmtList.Count());
+            MessageBox.Show("Finish Product");
+            for (int i = 0; i < cmtList.Count(); i++)
+            {
+                MessageBox.Show(cmtList[i].StartTime.ToString());
             }
         }
 
@@ -151,6 +157,7 @@ namespace TutorMaster
                 if (begin == date[i])
                 {
                     found = true;
+                    MessageBox.Show(date[i].ToString() + " is already in the database, this will not be added");
                 }
             }
             return found;
@@ -359,6 +366,120 @@ namespace TutorMaster
             lvSaturday.Columns.Add("Tutoring", 75);
             lvSaturday.Columns.Add("Weekly", 75);
             lvSaturday.Columns.Add("Partner", 75);
+        }
+
+
+        private void Split(ref List<TutorMaster.Commitment> values, int first, int last, ref int splitPoint)
+        {
+            int center = (first + last) / 2;
+            int median = 0;
+            DateTime valueF = Convert.ToDateTime(values[first].StartTime);
+            DateTime valueC = Convert.ToDateTime(values[center].StartTime);
+            DateTime valueL = Convert.ToDateTime(values[last].StartTime);
+
+            if ((DateTime.Compare(valueF, valueC) >= 0 && DateTime.Compare(valueF, valueL) <= 0) ||
+                (DateTime.Compare(valueF, valueL) >= 0 && DateTime.Compare(valueF, valueL) <= 0))
+            {
+                median = first;
+            }
+            else if (DateTime.Compare(valueC, valueF) >= 0 && (DateTime.Compare(valueC, valueL) <= 0) ||
+                   (DateTime.Compare(valueC, valueF)) >= 0 && (DateTime.Compare(valueC, valueL) <= 0))
+            {
+                median = center;
+            }
+            else
+            {
+                median = last;
+            }
+            /*Swap the median and first committments in the list*/
+            TutorMaster.Commitment temp = values[first];
+            values[first] = values[median];
+            values[median] = temp;
+
+            valueF = Convert.ToDateTime(values[first].StartTime); //get current first datetime
+            valueC = Convert.ToDateTime(values[center].StartTime);//get current center datetime;
+            valueL = Convert.ToDateTime(values[last].StartTime);
+
+            TutorMaster.Commitment splitVal = values[first];
+            DateTime splitDate =  Convert.ToDateTime(values[first].StartTime);
+            
+            int saveFirst = first;
+            bool onCorrectSide = true;
+
+            first++;
+            valueF = Convert.ToDateTime(values[first].StartTime);
+            do
+            {
+                onCorrectSide = true;
+                while (onCorrectSide)
+                {
+                    if (DateTime.Compare(valueF, splitDate) > 0)
+                    {
+                        onCorrectSide = false;
+                    }
+                    else
+                    {
+                        first++;
+                        valueF = Convert.ToDateTime(values[first].StartTime);
+                        onCorrectSide = (first <= last);
+                    }
+                }
+                
+                onCorrectSide = (first <= last);
+                while (onCorrectSide)
+                {
+                    if (DateTime.Compare(valueL, splitDate) <= 0)
+                    {
+                        onCorrectSide = false;
+                    }
+                    else
+                    {
+                        last--;
+                        valueL = Convert.ToDateTime(values[last].StartTime);
+                        onCorrectSide = (first <= last);
+                    }
+                }
+
+                if (first < last)
+                {
+                    TutorMaster.Commitment temp2 = values[first];
+                    values[first] = values[last];
+                    values[last] = temp2;
+                    first++;
+                    last--;
+
+                    valueF = Convert.ToDateTime(values[first].StartTime);
+                    valueL = Convert.ToDateTime(values[last].StartTime);
+                }
+            } while (first <= last);
+            
+            splitPoint = last;
+            TutorMaster.Commitment temp3 = values[saveFirst];
+            values[saveFirst] = values[splitPoint];
+            values[splitPoint] = temp3;
+            MessageBox.Show("Finish number: " + count.ToString());
+            for (int i = 0; i < values.Count(); i++)
+            {
+                MessageBox.Show(values[i].StartTime.ToString());
+            }
+
+        }
+
+        private void QuickSort2(ref List<TutorMaster.Commitment> values, int first, int last)
+        {
+            if (first < last)
+            {
+                int splitPoint = -99;
+
+                Split(ref values, first, last, ref splitPoint);
+                QuickSort2(ref values, first, splitPoint-1);
+                QuickSort2(ref values, splitPoint + 1, last);
+            }
+        }
+
+        private void QuickSort(ref List<TutorMaster.Commitment> values, int numValues)
+        {
+            QuickSort2(ref values, 0, numValues - 1);
         }
     }
 }
