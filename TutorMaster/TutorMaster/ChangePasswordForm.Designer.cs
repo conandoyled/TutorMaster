826a2603ff574c30e056a3620d67e35addfa491e
unknown <asmarek@SH301B-02.coe.int>
2017-04-20 13:16:22 -0500

    Change Password and Sign up are also pretty

--- TutorMaster/TutorMaster/ChangePasswordForm.Designer.cs
+++ TutorMaster/TutorMaster/ChangePasswordForm.Designer.cs
@@ -36,43 +36,47 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.btnCancel = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // txtOldPass
             // 
-            this.txtOldPass.Location = new System.Drawing.Point(24, 54);
+            this.txtOldPass.Location = new System.Drawing.Point(31, 29);
             this.txtOldPass.Name = "txtOldPass";
             this.txtOldPass.Size = new System.Drawing.Size(100, 20);
             this.txtOldPass.TabIndex = 0;
             // 
             // txtNewPass
             // 
-            this.txtNewPass.Location = new System.Drawing.Point(24, 96);
+            this.txtNewPass.Location = new System.Drawing.Point(31, 71);
             this.txtNewPass.Name = "txtNewPass";
             this.txtNewPass.Size = new System.Drawing.Size(100, 20);
             this.txtNewPass.TabIndex = 1;
             // 
             // txtConfirm
             // 
-            this.txtConfirm.Location = new System.Drawing.Point(24, 139);
+            this.txtConfirm.Location = new System.Drawing.Point(31, 114);
             this.txtConfirm.Name = "txtConfirm";
             this.txtConfirm.Size = new System.Drawing.Size(100, 20);
             this.txtConfirm.TabIndex = 2;
             // 
             // btnChangePassword
             // 
-            this.btnChangePassword.Location = new System.Drawing.Point(24, 181);
+            this.btnChangePassword.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(222)))), ((int)(((byte)(229)))));
+            this.btnChangePassword.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnChangePassword.Location = new System.Drawing.Point(60, 152);
             this.btnChangePassword.Name = "btnChangePassword";
-            this.btnChangePassword.Size = new System.Drawing.Size(100, 23);
+            this.btnChangePassword.Size = new System.Drawing.Size(146, 23);
             this.btnChangePassword.TabIndex = 3;
             this.btnChangePassword.Text = "Submit";
-            this.btnChangePassword.UseVisualStyleBackColor = true;
+            this.btnChangePassword.UseVisualStyleBackColor = false;
             this.btnChangePassword.Click += new System.EventHandler(this.btnChangePassword_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(130, 57);
+            this.label1.Location = new System.Drawing.Point(137, 32);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(72, 13);
             this.label1.TabIndex = 4;
@@ -81,7 +85,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(130, 103);
+            this.label2.Location = new System.Drawing.Point(137, 77);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(78, 13);
             this.label2.TabIndex = 5;
@@ -90,7 +94,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(130, 146);
+            this.label3.Location = new System.Drawing.Point(137, 121);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(116, 13);
             this.label3.TabIndex = 6;
@@ -98,19 +102,33 @@
             // 
             // btnCancel
             // 
-            this.btnCancel.Location = new System.Drawing.Point(133, 181);
+            this.btnCancel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(222)))), ((int)(((byte)(229)))));
+            this.btnCancel.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnCancel.Location = new System.Drawing.Point(60, 181);
             this.btnCancel.Name = "btnCancel";
-            this.btnCancel.Size = new System.Drawing.Size(113, 23);
+            this.btnCancel.Size = new System.Drawing.Size(146, 23);
             this.btnCancel.TabIndex = 7;
             this.btnCancel.Text = "Cancel";
-            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.UseVisualStyleBackColor = false;
             this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::TutorMaster.Properties.Resources.WatermarkR2;
+            this.pictureBox1.Location = new System.Drawing.Point(209, 155);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(67, 69);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 34;
+            this.pictureBox1.TabStop = false;
             // 
             // ChangePasswordForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(265, 223);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(218)))), ((int)(((byte)(232)))), ((int)(((byte)(239)))));
+            this.ClientSize = new System.Drawing.Size(274, 223);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -119,8 +137,10 @@
             this.Controls.Add(this.txtConfirm);
             this.Controls.Add(this.txtNewPass);
             this.Controls.Add(this.txtOldPass);
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(61)))), ((int)(((byte)(71)))), ((int)(((byte)(76)))));
             this.Name = "ChangePasswordForm";
             this.Text = "ChangePasswordForm";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -136,5 +156,6 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
--- TutorMaster/TutorMaster/CreateStudent.Designer.cs
+++ TutorMaster/TutorMaster/CreateStudent.Designer.cs
@@ -240,7 +240,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(218)))), ((int)(((byte)(232)))), ((int)(((byte)(239)))));
-            this.ClientSize = new System.Drawing.Size(242, 407);
+            this.ClientSize = new System.Drawing.Size(243, 407);
             this.Controls.Add(this.lblTClasses);
             this.Controls.Add(this.tvClasses);
             this.Controls.Add(this.txtPhoneNumber);
